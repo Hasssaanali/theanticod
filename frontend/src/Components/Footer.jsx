9cd844c035b3299b51c9649f0d3d2e8b7ca2f746
Hasssaanali <syedhasssaaan@gmail.com>
2022-09-15 13:44:29 +0500

    doneee

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Fragment } from 'react'
 import Typewriter from 'typewriter-effect';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function Footer() {
   return (
